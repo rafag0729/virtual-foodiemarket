e70736ad3c615505b9903f8d6a4c59d6020911ce
Rafael Gallego <rafaga_729@hotmail.com>
2020-08-03 08:39:06 -0500

    Adding the rest of the inlude files

---
+++ includes/banner.php
@@ -0,0 +1,29 @@
+<section class="banner"> <!--BANNER-->
+              <ul>
+                <li>
+                  <div class="carnes-frias">
+                    <p><span>Cero filas</span> en supermercados</p>
+                  </div>
+                </li>
+                <li>
+                  <div class="huevos">
+                    <p>Entregamos en la <span>puerta</span> de <span>tu casa</span></p>
+                  </div>
+                </li>
+                <li>
+                  <div class="maiz">
+                    <p>Paga <span>contraentrega</span></p>
+                  </div>
+                </li>
+                <li>
+                  <div class="pollo">
+                    <p><span>Ahorra</span> dinero</p>
+                  </div>
+                </li>
+                <li>
+                  <div class="quesos">
+                    <p>Alimentos <span>frescos</span></p>
+                  </div>
+                </li>
+              </ul>
+            </section>
---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<footer class="content-wrap"> <!--FOOTER-->
+              <div class="pqr">
+                <p>sugerencias</p>
+                <p>terminos de uso</p>
+                <p>politicas de privacidad</p>
+              </div>
+              <div class="copyright">
+                <p>2020. todos los derechos reservados ©</p>
+              </div>
+          </footer>
+
+          <script type="module" src="script/main.js"></script>
+      </body>
+</html>
