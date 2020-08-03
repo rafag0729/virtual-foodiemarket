6c67f84f4176d430f21b52a9942d6079692645f4
Rafael Gallego <rafaga_729@hotmail.com>
2020-08-03 08:38:23 -0500

    This is the include for the header.php

---
+++ includes/header.php
@@ -0,0 +1,48 @@
+<?php
+/*
+    require_once 'actions/conexion.php';
+    require_once 'actions/helpful-functions.php';
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+      <head>
+          <meta charset="utf-8">
+          <meta name="author" content="https://github.com/Rafa0729">
+          <meta name="description" content="miahorro.com es un supermercado virtual que ofrece la opción de hacer tus compras desde la comodidad de tu casa o tu celular, pide tus combos y paga contraentrega">
+          <meta name="keywords" content="mercado, alimentacion, productos, canasta, familiar">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>miahorro.com</title>
+          <link type="text/css" rel="stylesheet" href="styles/styles.css">
+          <script src="https://kit.fontawesome.com/98514e6d67.js" crossorigin="anonymous"></script>
+      </head>
+      <body>
+          <header class="content-wrap">
+              <div class="header-content">
+                <a class="logo" href="index.php">
+                    <img src="img/logo.png" alt="miahorro.com">
+                </a>
+                <nav>
+                    <div class="menu-icons">
+                      <div class="close hider">
+                          <i class="fas fa-times"></i>
+                      </div>
+                      <div class="bars">
+                          <i class="fas fa-bars"></i>
+                      </div>
+                    </div>
+                    <div class="relative-cont-links">
+                      <div class="menu-links hider">
+                        <a href="index.php">inicio</a>
+                        <a>quienes somos</a>
+                        <a href="contacto.php">contacto</a>
+                        <a class="cart-icon" href="carrito.php">
+                          <img src="img/cart.png" alt="Carrito">
+                        </a>
+                      </div>
+                    </div>
+                </nav>
+              </div>
+              <p>TU MERCADO EN LA PUERTA DE TU CASA</p>
+          </header>
